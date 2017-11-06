321655c7f3bf904fd7b1f003484049dc9ea61df1
Manas Bajaj <manas.bajaj94@gmail.com>
2017-11-06 21:43:11 +0000

    Multi field sign up sheet component added.

---
+++ forms/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import SignupSheet from './components/signup-sheet';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div style={{padding: "20px"}}>
+        <SignupSheet />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ forms/src/components/signup-sheet.js
@@ -0,0 +1,55 @@
+
+import React, { Component } from 'react';
+
+export default class SignupSheet extends Component {
+
+    state = {
+        fields: {
+            name: '',
+            email: ''
+        },
+        people: []
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Sign-up Sheet</h1>
+                <form onSubmit={this.onFormSubmit}>
+                    <input value={this.state.fields.name} placeholder="Name" name='name' onChange={this.onFieldChange}
+                        style={{ display: "block" }} />
+                    <input value={this.state.fields.email} placeholder="Email" name='email' onChange={this.onFieldChange}
+                        style={{ display: "block" }} />
+                    <br />
+                    <button type="submit" disabled={!this.isFormValid()}>Submit</button>
+                </form>
+                <br />
+                <ol>
+                    {this.state.people.map((person, idx) => <li key={idx}>{`${person.name} (${person.email})`}</li>)}
+                </ol>
+            </div>
+        );
+    }
+
+    onFieldChange = (event) => {
+        const fields = {};
+        fields[event.target.name] = event.target.value;
+        this.setState({fields: Object.assign(this.state.fields, fields)});
+    }
+
+    isFormValid = () => this.state.fields.name.length > 0 && this.state.fields.email.length > 0;
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        this.setState(prevState => {
+            return {
+                fields: {
+                    name: '',
+                    email: ''
+                },
+                people: prevState.people.concat([prevState.fields])
+            }
+        });
+    }
+}   
