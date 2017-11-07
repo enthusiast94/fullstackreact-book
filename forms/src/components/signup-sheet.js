25429e603806d94d5d4e19f791c1bc305aaf9ad2
Manas Bajaj <manas.bajaj94@gmail.com>
2017-11-07 22:54:20 +0000

    Added form validation.

--- forms/src/components/signup-sheet.js
+++ forms/src/components/signup-sheet.js
@@ -1,5 +1,5 @@
-
 import React, { Component } from 'react';
+import _ from 'lodash';
 
 export default class SignupSheet extends Component {
 
@@ -9,7 +9,12 @@ export default class SignupSheet extends Component {
             email: '',
             phoneNumber: ''
         },
+        fieldErrors: {},
         people: []
+    }
+
+    componentDidMount() {
+        this.validateForm();
     }
 
     render() {
@@ -17,12 +22,13 @@ export default class SignupSheet extends Component {
             <div>
                 <h1>Sign-up Sheet</h1>
                 <form onSubmit={this.onFormSubmit}>
-                    <input value={this.state.fields.name} placeholder="Name" name='name' onChange={this.onFieldChange}
-                        style={{ display: "block" }} />
-                    <input value={this.state.fields.email} placeholder="Email" name='email' onChange={this.onFieldChange}
-                        style={{ display: "block" }} />
-                    <input value={this.state.fields.phoneNumber} placeholder="Phone number" name='phoneNumber'
-                        onChange={this.onFieldChange} style={{ display: "block" }} type='tel' />
+                    <ErrorableInputField value={this.state.fields.name} placeholder="Name" name='name'
+                        onChange={this.onFieldChange} error={this.state.fieldErrors.name} />
+                    <ErrorableInputField value={this.state.fields.email} placeholder="Email" name='email'
+                        onChange={this.onFieldChange} error={this.state.fieldErrors.email} />
+                    <ErrorableInputField value={this.state.fields.phoneNumber} placeholder="Phone number"
+                        name='phoneNumber' onChange={this.onFieldChange} type='tel'
+                        error={this.state.fieldErrors.phoneNumber} />
                     <br />
                     <button type="submit" disabled={!this.isFormValid()}>Submit</button>
                 </form>
@@ -38,11 +44,22 @@ export default class SignupSheet extends Component {
         const fields = {};
         fields[event.target.name] = event.target.value;
         this.setState({ fields: Object.assign(this.state.fields, fields) });
+
+        this.validateForm();
     }
 
-    isFormValid = () => this.state.fields.name.length > 0 &&
-        this.state.fields.email.length > 0 &&
-        this.state.fields.phoneNumber.length > 0;
+    validateForm = () => {
+        const fieldErrors = {};
+        Object.keys(this.state.fields).forEach(field => {
+            if (this.state.fields[field].length === 0) {
+                fieldErrors[field] = `${field} cannot be empty`;
+            }
+        });
+
+        this.setState({ fieldErrors: fieldErrors });
+    }
+
+    isFormValid = () => _.isEmpty(this.state.fieldErrors);
 
     onFormSubmit = (event) => {
         event.preventDefault();
@@ -58,4 +75,14 @@ export default class SignupSheet extends Component {
             }
         });
     }
-}   
+}
+
+function ErrorableInputField(props) {
+    return (
+        <div style={{ display: "block" }}>
+            <input value={props.value} name={props.name} type={props.type} placeholder={props.placeholder}
+                onChange={props.onChange} />
+            <span style={{ color: "red", fontSize: "small", marginLeft: "10px" }}>{props.error}</span>
+        </div>
+    );
+}
